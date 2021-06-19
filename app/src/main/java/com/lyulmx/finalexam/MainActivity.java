d930b827993652d3d9b27a108d6fbf90e4bbe30c
Ming Xuan⁶¹⁸ <983835745@qq.com>
2021-06-19 17:22:22 +0800

    添加SQLite数据库

--- app/src/main/java/com/lyulmx/finalexam/MainActivity.java
+++ app/src/main/java/com/lyulmx/finalexam/MainActivity.java
@@ -2,13 +2,63 @@ package com.lyulmx.finalexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView mainlv;
+    List<Map<String, Object>> list;
+    Cursor cursor;
+    SQLiteDatabase db;
+    MyOpenHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        list = new ArrayList<Map<String, Object>>();
         setContentView(R.layout.activity_main);
+        mainlv = findViewById(R.id.lv);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,list,R.layout.activity_main,
+                new String[]{"_id","name","phone"},
+                new int[]{R.id.idStu,R.id.stuId,R.id.stuName});
+        mainlv.setAdapter(adapter);
+    }
+
+    public void GetData(){
+
+        list.clear();
+
+        Map<String, Object> map = null;
+        if(cursor != null && cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                map = new HashMap<String, Object>();
+                int id = cursor.getInt(cursor.getColumnIndex("_id"));
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                String phone = cursor.getString(cursor.getColumnIndex("phone"));
+
+                map.put("_id",id);
+                map.put("name",name);
+                map.put("phone",phone);
+
+                list.add(map);
+            }
+        }
+        cursor.close();
+        db.close();
     }
 }
---
+++ app/src/main/java/com/lyulmx/finalexam/MyOpenHelper.java
@@ -0,0 +1,24 @@
+package com.lyulmx.finalexam;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    public MyOpenHelper(Context context) {
+        super(context, "student.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table if not exists users(_id integer primary key autoincrement," +
+                " Stuname varchar(10), Stuphone varchar(11), StuId varchar(12), StuApartment varchar(6), StuSex int(1))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
